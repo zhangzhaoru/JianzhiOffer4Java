b34f01579f9cb8110b40b07bee7543fee4f73875
zhangzhaoru <914413583@qq.com>
2021-06-02 10:09:14 +0800

    判断是否为对称二叉树
    Signed-off-by: zhangzhaoru <914413583@qq.com>

--- Array/src/com/zhangzhaoru/java/TreeUtil.java
+++ Array/src/com/zhangzhaoru/java/TreeUtil.java
@@ -200,6 +200,23 @@ public class TreeUtil {
         inOrder4LinkTree(root.right);
     }
 
+    // 判断二叉树是否为对称二叉树
+    public static Boolean isSymmetrical(TreeNode root) {
+        if(root==null){
+            return true;
+        }
+        return isMirror(root.left,root.right);
+    }
+
+    public static Boolean isMirror(TreeNode left,TreeNode right){
+        if(left==null && right==null){
+            return true;
+        }
+        if(left==null||right==null){
+            return false;
+        }
+        return left.val==right.val&&isMirror(left.left,right.right)&&isMirror(left.right,right.left);
+    }
 
 
 
